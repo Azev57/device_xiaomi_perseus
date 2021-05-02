64dfeb29a6c151462c8e465e8fa54c6b54ccc8d5
Azev57 <83536165+Azev57@users.noreply.github.com>
2021-05-02 18:23:19 +0200

    perseus: some build rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@
 
 DEVICE_PATH := device/xiaomi/perseus
 
+BUILD_BROKEN_DUP_RULES := true
+
+ALLOW_MISSING_DEPENDENCIES := true
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := perseus
 
@@ -17,7 +21,11 @@ TARGET_SCREEN_DENSITY := 440
 
 # Kernel
 BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 TARGET_KERNEL_CONFIG := perseus_defconfig
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Partitions
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
@@ -31,21 +39,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
-# HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-ifeq ($(BOARD_AVB_ENABLE), true)
-   BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
-   BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
-   BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
-   BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
-   BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-endif
-
 # SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/perseus/BoardConfigVendor.mk
